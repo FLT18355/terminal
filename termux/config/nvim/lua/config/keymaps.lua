b5936b12483d0fd72554282c40c8c59ef0291f26
FLT18355 <cm18750724707a@outlook.com>
2026-04-22 19:21:27 +0800

    A

--- termux/config/nvim/lua/config/keymaps.lua
+++ termux/config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<F2>", "<cmd>Neotree toggle<CR>", { desc = "Toggle file tree" })
