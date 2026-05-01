b523c2843e05742e73c4c744525ae57795c84d45
FLT18355 <cm18750724707a@outlook.com>
2026-05-02 06:52:27 +0800

    commit

--- nvim/.config/lua/plugins/colorscheme.lua
+++ nvim/.config/lua/plugins/colorscheme.lua
@@ -3,15 +3,62 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    optional = true,
     opts = {
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
-      transparent_background = false,
+      transparent_background = true,
       term_colors = true,
       integrations = {
-              neotree = true, -- 启用对 NeoTree 的集成支持
-              blink_cmp = true,
+        aerial = true,
+        diffview = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "sky",
+        },
+        noice = true,
+        -- overseer = true,
+        telescope = {
+          enabled = true,
+          -- style = "nvchad",
+        },
+        treesitter = true,
+        notify = true,
+        gitsigns = true,
+        flash = true,
+        blink_cmp = true,
+        mason = true,
+        snacks = true,
+      },
+      highlight_overrides = {
+        mocha = function(mocha)
+          return {
+            CursorLineNr = { fg = mocha.yellow },
+            TelescopeSelection = { bg = mocha.surface0 },
+            TelescopeSelectionCaret = { fg = mocha.yellow, bg = mocha.surface0 },
+            TelescopePromptPrefix = { fg = mocha.yellow },
+            FlashCurrent = { bg = mocha.peach, fg = mocha.base },
+            FlashMatch = { bg = mocha.red, fg = mocha.base },
+            FlashLabel = { bg = mocha.teal, fg = mocha.base },
+            NormalFloat = { bg = mocha.base },
+            FloatBorder = { bg = mocha.base },
+            FloatTitle = { bg = mocha.base },
+            RenderMarkdownCode = { bg = mocha.crust },
+            Pmenu = { bg = mocha.surface0 },
+            Comment = { bg = nil, style = {} },
+            Conditional = { style = { "underline" } },
+            Keyword = { style = { "bold" } },
+            Repeat = { style = { "bold" } },
+            statusline = { bg = nil },
+
+            DiagnosticUnderlineError = { style = { "undercurl", "bold" }, sp = mocha.red },
+            DiagnosticUnderlineWarn  = { style = { "undercurl" }, sp = mocha.yellow },
+            DiagnosticUnderlineInfo  = { style = { "undercurl" }, sp = mocha.blue },
+            DiagnosticUnderlineHint  = { style = { "undercurl" }, sp = mocha.teal },
+          }
+        end,
       },
     },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 }
